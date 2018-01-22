9cf832398ee61c20196f864609415472dc39134f
Mars Wong <marswong618@gmail.com>
2018-01-22 04:57:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index >= 0 && index < board.length
+  if position_taken?(board, index)
+    return false
+  else
     return true
   end
 end
